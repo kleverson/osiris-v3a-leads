78dedb3596c05bd957ad608ae4f753a04af6f34d
Kleverson Holanda <kleverson.holanda@gmail.com>
2025-08-14 20:55:06 -0300

    add framer

--- src/app/components/LayoutContent/index.tsx
+++ src/app/components/LayoutContent/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 
 export default function LayoutContent({
@@ -24,28 +25,44 @@ export default function LayoutContent({
         }`}
       >
         {!isFinish && (
-          <img src="/img/boca.png" alt="" className="lg:w-[416px] w-[127px]" />
+          <motion.img
+            src="/img/boca.png"
+            alt=""
+            className="lg:w-[416px] w-[127px]"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, ease: "easeOut" }}
+          />
         )}
 
-        <img
+        <motion.img
           src={isFinish ? "/img/coma_blue.png" : "/img/coma.png"}
           alt=""
           className="lg:w-[140px] w-[56px]"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, ease: "easeOut", delay: 0.2 }}
         />
       </div>
       <div className="content relative z-20 flex-1">{children}</div>
       {!isFinish && (
         <>
-          <img
+          <motion.img
             src="/img/core.png"
-            className="absolute right-0 top-[55%] translate-y-[-50%] w-[152px]   lg:w-[333px]  transition-all"
+            className="absolute right-0 top-[55%] translate-y-[-50%] w-[152px] lg:w-[333px] transition-all"
             alt=""
+            initial={{ opacity: 0, x: 50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1, delay: 0.4 }}
           />
-          <img
+          <motion.img
             src="/img/pimpao.png"
-            className="absolute left-0  bottom-0 md:bottom-[60px] h-48  lg:h-[433px] transition-all"
+            className="absolute left-0 bottom-0 md:bottom-[60px] h-48 lg:h-[433px] transition-all"
             alt=""
-          />
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1, delay: 0.6 }}
+          />{" "}
         </>
       )}
     </div>
