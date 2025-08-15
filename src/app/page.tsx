8eabf3bce99fbc1486df84bfc8681945a8487bed
Kleverson Holanda <kleverson.holanda@gmail.com>
2025-08-15 09:06:56 -0300

    testing without brand bbseg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,38 @@
-"use client";
-import Button from "./components/Button";
-import { useRouter } from "next/navigation";
+'use client';
+import Button from './components/Button';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  const router = useRouter();
-  return (
-    <div className="wrapper max-h-screen flex flex-col justify-between">
-      <div className="flex items-start justify-start p-6 md:p-20 gap-16 w-full flex-1 pt-[50px]">
-        <main className="flex flex-col row-start-2 items-start sm:items-start w-4/5">
-          <h2 className="text-4xl md:text-7xl text-[#F1EA3C] mb-2 leading-none font-['BancoDoBrasilTitle'] font-extrabold">
-            Queremos <br /> te conhecer
-            <br /> melhor
-          </h2>
-          <p className="text-base md:text-2xl text-white font-light">
-            Fale um pouco sobre você e receba
-            <br /> novidades que realmente importam.
-          </p>
-        </main>
-      </div>
-      <div className="absolute w-full bottom-0">
-        <div className="my-10 mx-20 justify-end flex">
-          <img
-            src="/img/BEBESEG.png"
-            className="absolute right-6 md:right-[80px] translate-y-[-50%] w-[139px]  h-[27px] md:w-[230px] md:h-[40px] transition-all"
-            alt=""
-          />
+    const router = useRouter();
+    return (
+        <div className="wrapper max-h-screen flex flex-col justify-between">
+            <div className="flex items-start justify-start p-6 md:p-20 gap-16 w-full flex-1 pt-[50px]">
+                <main className="flex flex-col row-start-2 items-start sm:items-start w-4/5">
+                    <h2 className="text-4xl md:text-7xl text-[#F1EA3C] mb-2 leading-none font-['BancoDoBrasilTitle'] font-extrabold">
+                        Queremos <br /> te conhecer
+                        <br /> melhor
+                    </h2>
+                    <p className="text-base md:text-2xl text-white font-light">
+                        Fale um pouco sobre você e receba
+                        <br /> novidades que realmente importam.
+                    </p>
+                </main>
+            </div>
+            <div className="absolute w-full bottom-0">
+                {/* <div className="mx-20 justify-end flex">
+                    <img
+                        src="/img/BEBESEG.png"
+                        className="absolute right-6 md:right-[80px] translate-y-[-50%] w-[139px]  h-[27px] md:w-[230px] md:h-[40px] transition-all"
+                        alt=""
+                    />
+                </div> */}
+                <Button
+                    text={'Cadastro'}
+                    onClick={() => router.push('/register')}
+                    theme="yellow"
+                    sideArrow="right"
+                />
+            </div>
         </div>
-        <Button
-          text={"Cadastro"}
-          onClick={() => router.push("/register")}
-          theme="yellow"
-          sideArrow="right"
-        />
-      </div>
-    </div>
-  );
+    );
 }
